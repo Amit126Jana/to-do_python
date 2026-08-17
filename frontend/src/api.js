cd481940fcc2bdc0df536acf34d9b64d36c638b9
Amit Jana <webgrity126@gmail.com>
2026-08-17 12:31:08 +0530

    update 1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL || "https://to-do-python-zeta.vercel.app";
 
 function getToken() {
   return localStorage.getItem("token");
